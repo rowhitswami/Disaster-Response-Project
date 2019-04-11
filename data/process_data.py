6edd63e1900c331ec89d0da3d44c57aee2529d4c
Rohit Swami <rowhitswami1@gmail.com>
2019-04-11 16:37:27 +0530

    Migrated code from notebook

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,84 @@
 import sys
-
+import pandas as PD
+import sqlalchemy as DB
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    Loading the 'Messages' and 'Categories' datasets and
+    merging on common feature 'id'
+
+    INPUT: 
+        messages_filepath - path of CSV file
+        categories_filepath - path of CSV file
+    OUTPUT:
+        df - Merged dataframe
+    '''
+    # Loading Message dataset
+    messages = PD.read_csv(messages_filepath)
+
+    # Loading Categories dataset
+    categories = PD.read_csv(categories_filepath)
+
+    # Merge both datasets
+    df = PD.merge(messages, categories, on='id')
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    Flattening, cleaning and concatenating the 'categories' features with df
+    INPUT:
+        df - Merged dataframe
+    OUTPUT:
+        df - Cleaned dataframe
+    '''
+
+    # Split categories into separate category columns.
+    categories = df['categories'].str.split(';', expand=True)
+    col_values = categories.iloc[0].str.replace('-','')
+    col_values = col_values.str.replace('0','')
+    col_values = col_values.str.replace('1','')
+    categories.columns = list(col_values)
+
+    # Convert category values to just numbers 0 or 1.
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1:]
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # Droppping the original categories column from `df`
+    df = df.drop('categories', axis=1)
+ 
+    if df.shape[0] == categories.shape[0]:
+        print("Both dataframes can be concatenated.")
+    else:
+        print("Fix the shapes of the dataframe")
+    # Concatenating the original dataframe with the new `categories` dataframe
+    df = PD.concat([df, categories], axis=1)
+
+    # As the 'message' column contains the translated messages,
+    # it is safe to remove the 'original' column from dataset 
+    # considering its count of null values
+    df = df.drop('original', axis=1)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    Save the clean dataset into an sqlite database
+
+    INPUT:
+        df - Cleaned dataframe
+        database_filename - Name of database
+    OUTPUT:
+        SQL Database
+    '''
+
+    engine = DB.create_engine('sqlite:///' + database_filename)
+    df.to_sql('myTable', engine, index=False)
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,31 +1,85 @@
+# import libraries
 import sys
+import pandas as pd
+import sqlalchemy as DB
+import nltk
+import pickle
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize 
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 def load_data(database_filepath):
-    pass
+    # load data from database
+    engine = DB.create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql('select * from myTable', con=engine)
+    X = df['message'].values
+    Y = df.drop(['id', 'message', 'genre'], axis=1)
+    return X, Y
 
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+
+    # Creating pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+
+        # Experimented with different algorithms and
+        # AdaBoostClassifier gave the best result
+        ('clf', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+
+    return pipeline
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+def evaluate_model(model, X_test, Y_test):
+    Y_pred = model.predict(X_test)
+
+    # Overall Accuracy
+    overall_accuracy = (Y_pred == Y_test).mean().mean()
+    print('OVERALL ACCURACY OF MODEL: ', overall_accuracy)
+
+    Y_pred_df = pd.DataFrame(Y_pred, columns = Y_test.columns)
+    for column in Y_test.columns:
+        print('------------------------------------------------------\n')
+        print('Feature: {}\n'.format(column))
+        print('Accuracy: ', accuracy_score(Y_test[column], Y_pred_df[column]))
+        print(classification_report(Y_test[column],Y_pred_df[column]))
 
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
+        X, Y = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
         print('Building model...')
@@ -35,7 +89,7 @@ def main():
         model.fit(X_train, Y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, Y_test)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
